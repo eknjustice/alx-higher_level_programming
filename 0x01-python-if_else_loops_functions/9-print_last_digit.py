f289a6979e04d05306b690f5b7d845c0ead77425
eknjustice <eknjustice@gmail.com>
2022-12-01 06:36:42 +0200

    string indexing and str casting

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+def print_last_digit(number):
+        number_as_string = str(number)
+        last_digit_as_string = number_as_string[-1]
+        last_digit = int(last_digit_as_string)
+        print(last_digit)
