3691412d49eb9dcdee7bd0e916552f0b68775db2
eknjustice <eknjustice@gmail.com>
2022-12-01 07:18:50 +0200

    ord(i)-32 and char(ord(i))

--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 uppercase = __import__('8-uppercase').uppercase
 
-uppercase("holberton\n")
-uppercase("Holberton School\n")
-uppercase("Holberton School 98 Battery street\n")
-uppercase(""'\n')
-uppercase('z\n')
+uppercase("best\n")
+uppercase("Best School 98 Battery street\n")
