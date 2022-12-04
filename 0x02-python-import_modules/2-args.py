b1f5e1298bfbd6c00c7bf1fddb0eef7b5d418277
eknjustice <eknjustice@gmail.com>
2022-12-04 15:03:28 +0200

    sys.argv, len(argv), print and else if statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,15 +4,15 @@ if __name__ == "__main__":
 
 argv = sys.argv
 n = len(argv) - 1
-    
+i = 1
 if n == 0:
     print("{:d} argument.".format(n))
 else:
     if n == 1:
         print("{:d} argument:".format(n))
+        print("{:d}: {:s}".format(i, argv[i]))
     else:
         print("{:d} arguments:".format(n))
-        i = 1
-    while i <= n:
-        print("{:d}: {:s}".format(i, argv[i]))
-        i += 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
