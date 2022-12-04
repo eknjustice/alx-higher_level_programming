0bc5b93f056a875373796c9fbaad1406f2f89f5b
eknjustice <eknjustice@gmail.com>
2022-12-04 16:31:42 +0200

    print words from compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
 def alphabet():
+
     words = dir(hidden_4)
+    
     for i in words:
         if i[:2] != "__":
             print(i)
+
 if __name__ == "__main__":
     alphabet()
