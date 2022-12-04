eb526409d2bf6fb0d1a2645e16ba83ca6f49b026
eknjustice <eknjustice@gmail.com>
2022-12-04 17:15:38 +0200

    print members of a list using a loop

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print('{:d}'.format(i))
+    n = len(my_list)
+    i = 0
+    while i < n:
+        print('{:d}'.format(my_list[i]))
+        i += 1
